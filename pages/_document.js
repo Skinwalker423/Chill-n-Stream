4f503a2b7d473f03fdb44785b339b4e81678f361
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2022-10-06 22:16:25 -0700

    Chill'n Stream

---
+++ pages/_document.js
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+return (
+<Html>
+    <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap" rel="stylesheet"></link>
+        <meta name='description' content="Chill n Stream" />
+    </Head>
+    <body>
+    <Main />
+    <NextScript />
+    </body>
+</Html>
+)
+}
