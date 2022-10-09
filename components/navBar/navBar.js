8a89ec010b0ef127a83ebbfeb2bc57d2d883f7aa
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2022-10-09 01:22:14 -0700

    sign in/out

--- components/navBar/navBar.js
+++ components/navBar/navBar.js
@@ -3,12 +3,26 @@ import Image from 'next/image'
 import { useState } from 'react';
 
 
-const NavBar = ({logoUrl, username, avatarUrl, setExpand}) => {
+const NavBar = ({logoUrl, username, avatarUrl}) => {
 
+  const [expand, setExpand] = useState(false);
+  const [user, setUser] = useState(null);
 
   const handleUserInfo = () => {
     setExpand((bool) => !bool);
-  }  
+  }
+  const handleSignOut = () => {
+    console.log('logging off');
+    setUser(null);
+    setExpand((bool) => !bool);
+    
+  }
+
+  const handleSignIn = () => {
+    setUser('423@gmail.com')
+  }
+  
+  
 
   return (
     <div className={styles.navContainer}>
@@ -16,12 +30,18 @@ const NavBar = ({logoUrl, username, avatarUrl, setExpand}) => {
             <h1 className={styles.header}>Netflix</h1>
             <p>My List</p>
         </div>
-        <button onClick={handleUserInfo} className={styles.userContainer}>
-            <span className={styles.username}>{username}</span>
-            <div className={styles.expandIconWrapper}>
-                <Image src={avatarUrl} width={20} height={20} />  
-            </div> 
-        </button>
+        <nav>
+            <div>
+                {user ? <button onClick={handleUserInfo} className={styles.userContainer}>
+                    <span className={styles.username}>{username}</span>
+                    <div className={styles.expandIconWrapper}>
+                        <Image src={avatarUrl} width={20} height={20} />  
+                    </div> 
+                </button> : <button className={styles.signInButton} onClick={handleSignIn}>Sign In</button>}
+                {user && expand && <button onClick={handleSignOut} type='button' className={styles.accountDetails}>Sign Out</button>}
+            </div>
+            
+        </nav>
     </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,9 @@ import Head from 'next/head'
 import Banner from '../components/banner/Banner'
 import styles from '../styles/Home.module.css'
 import NavBar from '../components/navBar/navBar'
-import { useState } from 'react'
 
 export default function Home() {
 
-  const [expand, setExpand] = useState(false);
 
   return (
     <div className={styles.container}>
@@ -16,10 +14,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <NavBar username={'423@gmail.com'} avatarUrl={'/static/expand.svg'} setExpand={setExpand} />
+        <NavBar username={'423@gmail.com'} avatarUrl={'/static/expand.svg'} />
         
         <Banner title="Clifford the red dog" subTitle="a very cute dog" buttonName="Play" imgUrl={'/static/clifford.webp'} />
-        {expand && <div className={styles.accountDetails}>Account Details</div>}
       </main>
     </div>
   )
