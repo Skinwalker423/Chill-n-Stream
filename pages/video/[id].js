a9b1421aeca5acef6ef7caf756ff2403b1cebddb
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2022-10-19 22:11:24 -0700

    adding modal for video

--- pages/video/[id].js
+++ pages/video/[id].js
@@ -1,23 +1,48 @@
+import React, {useState} from 'react';
+import { useRouter } from 'next/router';
 import styles from './[id].module.css'
+import Modal from 'react-modal';
+Modal.setAppElement('#__next');
+
 
 const Video = () => {
+
+    const router = useRouter();
+
+    let subtitle;
+    const [isOpen, setIsOpen] = useState(true);
+
+    function openModal() {
+        setIsOpen(true);
+    }
+
+    function afterOpenModal() {
+        // references are now sync'd and can be accessed.
+        subtitle.style.color = '#f00';
+    }
+
+    function closeModal() {
+        router.back();
+        setIsOpen(false);
+        // router.push('/');
+  }
+
     return (
         <div className={styles.container}>
-            <div className={styles.videoContainer}>
-                <div className={styles.videoWrapper}>
-                    <h1>Video Player</h1>
-                </div>
-                <div className={styles.detailsContainer}>
-                    <div>
-                        <h3>Left side details</h3>
-                        <p>description</p>
-                    </div>
-                    <div>
-                        <h3>Right side details</h3>
-                        <p>description</p>
-                    </div>
-                </div>
-            </div>
+            <Modal
+                isOpen={isOpen}
+                onAfterOpen={afterOpenModal}
+                onRequestClose={closeModal}
+                contentLabel="Chill n stream the movie"
+                className={styles.modal}
+                overlayClassName={styles.overlay}
+            >
+                <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Hello</h2>
+                <button onClick={closeModal}>close</button>
+                
+                <div>I am a modal</div>
+            </Modal>
+            
         </div>
     )
 }
