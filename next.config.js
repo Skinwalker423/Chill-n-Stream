ca772a2908fec0dec03f9d5e7f4d8f9a701941c5
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2022-10-20 22:09:01 -0700

    getStaticProps getStaticPaths

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
     domains: ['img.icons8.com', 'images.unsplash.com', 'i.ytimg.com']
-  }
+  },
 }
 
 module.exports = nextConfig
--- pages/video/[id].js
+++ pages/video/[id].js
@@ -3,9 +3,59 @@ import { useRouter } from 'next/router';
 import styles from './[id].module.css'
 import Modal from 'react-modal';
 Modal.setAppElement('#__next');
+import clsx from 'classnames';
+import { getVideoById } from '../../lib/fetchYouTubeVideos';
+
+export async function getStaticProps({params}) {
+  
+    const vidId = params.id;
+    // const VideoList = await getVideoById(vidId);
+
+  return {
+    props: {
+      video: {},
+    },
+    // Next.js will attempt to re-generate the page:
+    // - When a request comes in
+    // - At most once every 10 seconds
+    revalidate: 60, // In seconds
+  }
+}
+
+// This function gets called at build time on server-side.
+// It may be called again, on a serverless function, if
+// the path has not been generated.
+export async function getStaticPaths() {
+//   const res = await fetch('https://.../posts')
+//   const posts = await res.json()
+const listofBannerVids = ["mYfJxlgR2jw", "4zH5iYM4wJo", "KCPEHsAViiQ"];
+
+  // Get the paths we want to pre-render based on posts
+  const paths = listofBannerVids.map((vidId) => ({
+    params: { id: vidId },
+  }))
+
+  // We'll pre-render only these paths at build time.
+  // { fallback: blocking } will server-render pages
+  // on-demand if the path doesn't exist.
+  return { paths, fallback: 'blocking' }
+}
 
 
-const Video = () => {
+
+const Video = (initialProps) => {
+
+    const videoBackup = {
+        title: "Hi cute dog",
+        publishTime: "1990-01-01",
+        description: "A big red dog that is super cute, can he get any bigger?",
+        channelTitle: "Paramount Pictures",
+        statistics: {
+            viewCount: 10000
+        },
+    };
+
+    const { title, publishTime, description, channelTitle, statistics: {viewCount} } = videoBackup;
 
     const router = useRouter();
 
@@ -50,15 +100,23 @@ const Video = () => {
                     className={styles.videoPlayer}
                     
                 ></iframe>
-                
-                <div className={styles.detailsContainer}>
-                    <div>
-                        <h2>Title</h2>
-                        <p>left side description</p>
-                    </div>
-                    <div>
-                        <h2>Title</h2>
-                        <p>right side description</p>
+                <div className={styles.modalBody}>
+                    <div className={styles.modalBodyContent}>
+                        <div className={styles.col1}>
+                            <p className={styles.publishTime}>{publishTime}</p>
+                            <p className={styles.title}>{title}</p>
+                            <p className={styles.description}>{description}</p>
+                        </div>
+                        <div className={styles.col2}>
+                            <p className={clsx(styles.subText, styles.subTextWrapper)}>
+                                <span className={styles.textColor}>Cast: </span>
+                                <span className={styles.channelTitle}>{channelTitle}</span>
+                            </p>
+                            <p className={clsx(styles.subText, styles.subTextWrapper)}>
+                                <span className={styles.textColor}>View Count: </span>
+                                <span className={styles.channelTitle}>{viewCount}</span>
+                            </p>
+                        </div>
                     </div>
                 </div>
             </Modal>
