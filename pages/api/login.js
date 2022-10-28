86209df50b6088d3f20061946167acb040afb207
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2022-10-27 22:45:18 -0700

    insert user

--- pages/api/login.js
+++ pages/api/login.js
@@ -4,16 +4,19 @@ const magicAdmin = new Magic(process.env.SECRET_MAGIC_LINK_API_KEY);
 // import { magicAdmin } from '../../lib/magic';
 import jwt from 'jsonwebtoken';
 import { startFetchMyQueryUserCheck } from '../../lib/db/hasura';
+import { startInsertUser } from '../../lib/db/hasura';
 
 const login = async(req, res) => {
     if(req.method === 'POST'){
         try{
             const auth = req.headers.authorization;
             const didToken = auth ? auth.slice(7) : '';
+            console.log({didToken});
+            console.log({auth});
 
             const metadata = await magicAdmin.users.getMetadataByToken(didToken);
             console.log("metadata:", metadata);
-            const {issuer, email, publicAdress} = metadata;
+            const {issuer, email, publicAddress} = metadata;
 
             if(metadata.issuer){
                 const token = jwt.sign({
@@ -24,8 +27,14 @@ const login = async(req, res) => {
                         ...metadata,
                     }
                 }, process.env.NEXT_PUBLIC_JWT_SECRET, { expiresIn: '7d' });
+
                 
                 const isNewUser = await startFetchMyQueryUserCheck(token, issuer);
+
+                
+                const newUser = await startInsertUser(email, issuer, publicAddress, token);
+                console.log({newUser});
+                
                 
                 res.send({done: true, isNewUser});
             }
